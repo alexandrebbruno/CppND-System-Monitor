085295fc3d7ced51d9004d32f739b5baabd99574
abarbosa <abarbosa@esss.co>
2021-03-25 22:17:26 -0300

    implement processor

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,25 @@
 #include "processor.h"
 
-// TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+#include "linux_parser.h"
+
+// Return the aggregate CPU utilization
+float Processor::Utilization() {
+  long total = LinuxParser::Jiffies();
+  long active = LinuxParser::ActiveJiffies();
+  long idle = LinuxParser::IdleJiffies();
+  
+  long total_old = total_;
+  long idle_old = idle_;
+  
+  float total_delta = total - total_old;
+  float idle_delta = idle - idle_old;
+  
+  float utilization = (total_delta - idle_delta)/total_delta;
+  
+  // Update
+  idle_ = idle;
+  total_ = total;
+  active_ = active;
+  
+  return utilization; 
+}
